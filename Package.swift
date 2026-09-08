b2bbc8f8c8496fe521d896fe2fdcb67013dca70a
Ulf Akerstedt-Inoue <hakkabon@gmail.com>
2026-09-08 16:29:11 +0900

    added new package and license file

--- Package.swift
+++ Package.swift
@@ -5,15 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "Swift-Lens",
+    platforms: [
+        .macOS(.v13), .iOS(.v16), .tvOS(.v16), .watchOS(.v9), .macCatalyst(.v16),
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "Swift-Lens",
-            targets: ["Swift-Lens"]),
+        .library(name: "Swift-Lens",targets: ["Swift-Lens"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "Swift-Lens"),
         .testTarget(
